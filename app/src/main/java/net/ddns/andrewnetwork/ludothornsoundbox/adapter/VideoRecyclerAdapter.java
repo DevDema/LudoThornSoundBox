ff8082a0512346e0b518f375b8f09e4ab7bcbcfa
DEMATTEISA <andrea.dematteis@linksmt.it>
2018-12-12 09:19:18 +0100

    New audios. Updated recyclerView.

--- app/src/main/java/net/ddns/andrewnetwork/ludothornsoundbox/adapter/VideoRecyclerAdapter.java
+++ app/src/main/java/net/ddns/andrewnetwork/ludothornsoundbox/adapter/VideoRecyclerAdapter.java
@@ -27,9 +27,21 @@ public class VideoRecyclerAdapter extends RecyclerView.Adapter {
     ParentFragment fragment;
     List<LudoVideo> videoList;
 
+    private static final int PROGRESS = 1;
+    private static final int VIDEO = 0;
+
     public VideoRecyclerAdapter(ParentFragment fragment, List<LudoVideo> videoList) {
         this.fragment = fragment;
         this.videoList = videoList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // Just as an example, return 0 or 2 depending on position
+        // Note that unlike in ListView adapters, types don't have to be contiguous
+        if(position==getItemCount()-1)
+            return PROGRESS;
+        else return VIDEO;
     }
 
     @NonNull
@@ -37,68 +49,74 @@ public class VideoRecyclerAdapter extends RecyclerView.Adapter {
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         LayoutInflater inflater = (LayoutInflater) Objects.requireNonNull(fragment.getContext()).getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.object_video, parent, false);
-        return new VideoViewHolder(v);
+        if(i == PROGRESS) {
+            v = inflater.inflate(R.layout.progress_bar_item, parent, false);
+            return new ProgressViewHolder(v);
+        }
+        else return new VideoViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
-        LudoVideo p = videoList.get(position);
-        if (p != null) {
-            TextView tt1 = viewHolder.itemView.findViewById(R.id.videotitle);
-            TextView tt2 = viewHolder.itemView.findViewById(R.id.videodesc);
-            TextView channel = viewHolder.itemView.findViewById(R.id.videochannel);
-            TextView likes = viewHolder.itemView.findViewById(R.id.likes);
-            TextView dislikes = viewHolder.itemView.findViewById(R.id.dislikes);
-            TextView views = viewHolder.itemView.findViewById(R.id.views);
-            TextView updated = viewHolder.itemView.findViewById(R.id.Updated);
-
-
-            ImageView tt3 = viewHolder.itemView.findViewById(R.id.icon);
-
-            if (tt1 != null) {
-                tt1.setText(p.getTitle());
-            }
-
-            if (tt2 != null) {
-                tt2.setText(p.getDescription());
-            }
-
-            if (channel != null) {
-                channel.setText(p.getChannel().getChannelName());
-            }
-            VideoInformationManager videoInformationManager = new VideoInformationManager(fragment, p);
-            if (likes != null) {
-                likes.setText(videoInformationManager.getCompactedLikes());
-            }
-
-            if (dislikes != null) {
-                dislikes.setText(videoInformationManager.getCompactedDislikes());
-            }
-
-            if (views != null) {
-                views.setText(videoInformationManager.getCompactedViews());
-
-            }
-
-            if (tt3 != null) {
-                Thumbnail thumbnail = p.getThumbnail();
-                Bitmap image = null;
-                if(thumbnail!=null) image=thumbnail.getImage();
-                if(image!=null) tt3.setImageBitmap(image);
-
-            }
-            if(updated!=null)
-                if(fragment.isTablet()) updated.setText(p.getDateTime().toExtendedString());
-                else updated.setText(p.getDateTime().toString());
-        }
-        viewHolder.itemView.setOnClickListener(v -> {
-            Uri uri = null;
+        if(viewHolder.getItemViewType() == 0) {
+            LudoVideo p = videoList.get(position);
             if (p != null) {
-                uri = Uri.parse(VideoManager.buildVideoUrl(p.getId()));
+                TextView tt1 = viewHolder.itemView.findViewById(R.id.videotitle);
+                TextView tt2 = viewHolder.itemView.findViewById(R.id.videodesc);
+                TextView channel = viewHolder.itemView.findViewById(R.id.videochannel);
+                TextView likes = viewHolder.itemView.findViewById(R.id.likes);
+                TextView dislikes = viewHolder.itemView.findViewById(R.id.dislikes);
+                TextView views = viewHolder.itemView.findViewById(R.id.views);
+                TextView updated = viewHolder.itemView.findViewById(R.id.Updated);
+
+
+                ImageView tt3 = viewHolder.itemView.findViewById(R.id.icon);
+
+                if (tt1 != null) {
+                    tt1.setText(p.getTitle());
+                }
+
+                if (tt2 != null) {
+                    tt2.setText(p.getDescription());
+                }
+
+                if (channel != null) {
+                    channel.setText(p.getChannel().getChannelName());
+                }
+                VideoInformationManager videoInformationManager = new VideoInformationManager(fragment, p);
+                if (likes != null) {
+                    likes.setText(videoInformationManager.getCompactedLikes());
+                }
+
+                if (dislikes != null) {
+                    dislikes.setText(videoInformationManager.getCompactedDislikes());
+                }
+
+                if (views != null) {
+                    views.setText(videoInformationManager.getCompactedViews());
+
+                }
+
+                if (tt3 != null) {
+                    Thumbnail thumbnail = p.getThumbnail();
+                    Bitmap image = null;
+                    if (thumbnail != null) image = thumbnail.getImage();
+                    if (image != null) tt3.setImageBitmap(image);
+
+                }
+                if (updated != null)
+                    if (fragment.isTablet()) updated.setText(p.getDateTime().toExtendedString());
+                    else updated.setText(p.getDateTime().toString());
             }
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            viewHolder.itemView.getRootView().getContext().startActivity(intent);
-        });
+            viewHolder.itemView.setOnClickListener(v -> {
+                Uri uri = null;
+                if (p != null) {
+                    uri = Uri.parse(VideoManager.buildVideoUrl(p.getId()));
+                }
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                viewHolder.itemView.getRootView().getContext().startActivity(intent);
+            });
+        }
     }
 
     @Override
@@ -108,6 +126,12 @@ public class VideoRecyclerAdapter extends RecyclerView.Adapter {
 
     public static class VideoViewHolder extends RecyclerView.ViewHolder {
         private VideoViewHolder(View v) {
+            super(v);
+        }
+    }
+
+    public static class ProgressViewHolder extends RecyclerView.ViewHolder {
+        private ProgressViewHolder(View v) {
             super(v);
         }
     }
