c9d79b82a5afaf4f4c9dc0d9119a0265dbfe91d2
DEMATTEISA <andrea.dematteis@linksmt.it>
2019-05-13 15:46:06 +0200

    Puliti appcostants.

--- app/src/main/java/net/ddns/andrewnetwork/ludothornsoundbox/utils/AppConstants.java
+++ app/src/main/java/net/ddns/andrewnetwork/ludothornsoundbox/utils/AppConstants.java
@@ -22,17 +22,11 @@ package net.ddns.andrewnetwork.ludothornsoundbox.utils;
 public final class AppConstants {
     public static String ADS_CODE = "ca-app-pub-3889032681139142~8858308706";
 
-    public static final String STATUS_CODE_SUCCESS = "success";
-    public static final String STATUS_CODE_FAILED = "failed";
-
-    public static final int API_STATUS_CODE_LOCAL_ERROR = 0;
-
     public static final String DB_NAME = "mindorks_mvp.db";
     public static final String PREF_NAME = "mindorks_pref";
     public static final long NULL_INDEX = -1L;
     public static final String LOOKUP_TYPE_VIDEO = "video";
     public static final String ORDER_TYPE_VIDEO = "date";
-    public static final String FILE_FRASI_RANDOM="frasi_random";
 
     private AppConstants() {
         // This utility class is not publicly instantiable
