983f421eb09b5bdec10463c4e5dd80ade52e26d1
DEMATTEISA <andrea.dematteis@linksmt.it>
2019-05-13 17:45:34 +0200

    Cominciata implementazione degli  ad.

--- app/src/main/java/net/ddns/andrewnetwork/ludothornsoundbox/ui/main/ParentActivity.java
+++ app/src/main/java/net/ddns/andrewnetwork/ludothornsoundbox/ui/main/ParentActivity.java
@@ -1,10 +1,6 @@
 package net.ddns.andrewnetwork.ludothornsoundbox.ui.main;
 
-import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 
 import com.google.ads.mediation.admob.AdMobAdapter;
@@ -15,22 +11,19 @@ import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
 import net.ddns.andrewnetwork.ludothornsoundbox.R;
-import net.ddns.andrewnetwork.ludothornsoundbox.ui.base.BaseActivity;
-import net.ddns.andrewnetwork.ludothornsoundbox.ui.base.NavigationDrawerActivity;
 import net.ddns.andrewnetwork.ludothornsoundbox.ui.base.PreferencesManagerActivity;
 import net.ddns.andrewnetwork.ludothornsoundbox.utils.AppConstants;
-
-import androidx.annotation.StyleRes;
 
 public abstract class ParentActivity extends PreferencesManagerActivity {
 
     public interface AdCustomListener {
 
         void onAdLoaded();
+
     }
 
     AdView mAdView;
-    InterstitialAd interstitialAd;
+    InterstitialAd mInterstitialAd;
     AdCustomListener adCustomListener;
 
     @Override
@@ -42,59 +35,42 @@ public abstract class ParentActivity extends PreferencesManagerActivity {
 
         mAdView = findViewById(R.id.adView);
         Bundle extras = new Bundle();
-        //extras.putString("max_ad_content_rating", "T");
+
         AdListener adListener = new AdListener() {
 
             @Override
             public void onAdLoaded() {
                 mAdView.setVisibility(View.VISIBLE);
-                if (adCustomListener != null)
+
+                if (adCustomListener != null) {
                     adCustomListener.onAdLoaded();
+                }
             }
 
             @Override
             public void onAdFailedToLoad(int error) {
                 mAdView.setVisibility(View.GONE);
-                if (adCustomListener != null)
+
+                if (adCustomListener != null) {
                     adCustomListener.onAdLoaded();
+                }
             }
 
             @Override
             public void onAdClosed() {
                 AdRequest adRequest = new AdRequest.Builder().build();
-                interstitialAd.loadAd(adRequest);
+                mInterstitialAd.loadAd(adRequest);
             }
 
         };
         mAdView.setAdListener(adListener);
-        interstitialAd = new InterstitialAd(this);
+        mInterstitialAd = new InterstitialAd(this);
 
-        interstitialAd.setAdUnitId("ca-app-pub-3889032681139142/3864341552");
-        interstitialAd.setAdListener(adListener);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3889032681139142/3864341552");
+        mInterstitialAd.setAdListener(adListener);
         mAdView.loadAd(new AdRequest.Builder().addNetworkExtrasBundle(AdMobAdapter.class, extras).build());
-        interstitialAd.loadAd(new AdRequest.Builder().addNetworkExtrasBundle(AdMobAdapter.class, extras).build());
+        mInterstitialAd.loadAd(new AdRequest.Builder().addNetworkExtrasBundle(AdMobAdapter.class, extras).build());
 
-    }
-
-
-
-    public int getDisplayHeight() {
-        return getSize().y;
-    }
-
-    public int getDisplayWidth() {
-        return getSize().x;
-    }
-
-    private Point getSize() {
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size;
-    }
-
-    public InterstitialAd getInterstitialAd() {
-        return interstitialAd;
     }
 
     @Override
